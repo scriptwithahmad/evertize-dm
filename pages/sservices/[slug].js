a8cccc4a8a4029a500ac216500b27bef1a41b522
Ahmad <scriptwithahmad@gmail.com>
2023-10-02 20:44:23 +0500

    url change

--- pages/sservices/[slug].js
+++ pages/sservices/[slug].js
@@ -57,7 +57,7 @@ export default singleService;
 
 export async function getServerSideProps({ params }) {
   const slug = params.slug;
-  const res = await fetch(`http://localhost:3000/api/addservice/${slug}`);
+  const res = await fetch(`https://evertize.vercel.app/api/addservice/${slug}`);
 
   const data = await res.json();
   return { props: { data } };
