9774edd9d0702c1f7586fac45bf50e9bf9a9c924
Ahmad <scriptwithahmad@gmail.com>
2023-10-16 19:11:10 +0500

    Hero Page Design Update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,34 +1,36 @@
 import React from "react";
+import Lottie from "lottie-react";
+import anime from "@/public/anime.json"
+import fb from "@/public/fb.json"
+import insta from "@/public/insta.json"
 
 const Hero = () => {
   return (
     <div className="bg-[#eee] z-10 relative overflow-hidden">
-      <div className="heroMain max-w-[1200px] m-auto h-full min-h-[90vh] flex  items-center justify-between">
-        <div>
-          <h1 className="font-extrabold text-[55px] leading-normal max-w-[90%] bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-transparent bg-clip-text">
-            Grow Your Bussiness With{" "}
+      <div className=" max-w-[1200px] m-auto h-full min-h-[100vh] flex items-center justify-center">
+        <div className="w-1/2 ">
+          <h1 className="font-extrabold text-[45px] leading-normal max-w-[80%] bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-transparent bg-clip-text">
             <span className="font-extrabold text-[#FA7909]"> EVERTIZE </span>
-            Marketing
+            Your Strategic Partner in Marketing Success
           </h1>
-          <p className="text-[18px] mb-4 leading-[1.8] text-gray-900 max-w-[75%]">
+          <p className="mb-4 leading-[1.4] text-gray-900 max-w-[60%]">
             We specialize in delivering comprehensive Marketing and Branding
             Solutions tailored to your needs.
           </p>
           <button className="btn"> Contact us </button>
         </div>
-        <img
-          className="mix-blend-darken w-[430px] relative top-[50%] z-[3333333333333333333]"
-          id="heroImg"
-          src="/heroshape.png"
-          alt="hero image"
-        />
+        <div className="relative w-1/2  ">
+          <Lottie  animationData={anime}/>
+          <Lottie className="absolute top-0 left-[15%] w-1/3" animationData={fb}/>
+          <Lottie className="absolute bottom-[20%] -z-30 right-[25%] bordef border-red-600  w-[5vw]" animationData={insta}/>
+
+        </div>
       </div>
       <img
         className="hero-svg-1 absolute top-[77%] left-[-6%] h-56 animate-pulse"
         src="/overlay.png"
         alt="overlay"
       />
-      {/* <div className="Hero-img-shape absolute top-[20%] right-24 z-[-1] shadow-2xl rounded-full bg-white h-72 w-72"></div> */}
       <img
         className="hero-svg-2 animateElm absolute top-[75%] left-[55%] mix-blend-multiply h-36"
         src="https://d2pas86kykpvmq.cloudfront.net/landings/squiggel/hero.png"
